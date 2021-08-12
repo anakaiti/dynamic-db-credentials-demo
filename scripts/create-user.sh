97e3417676bb532e2fbfb184d272791f32157126
Muhammad Yahya Harlan <muhammad.yahya@payfazz.com>
2021-08-12 11:39:32 +0700

    Format create-user output

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl http://localhost:3000/persons -H "Accept: application/json" -X POST --data '{"firstName": "John", "secondName": "Doe", "email": "john.doe@example.com"}'
 echo
-curl http://localhost:3000/persons
+curl http://localhost:3000/persons | jq
--- scripts/sql/init.sql
+++ scripts/sql/init.sql
@@ -1,4 +1,5 @@
 CREATE ROLE viewer;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO viewer;
+
 ALTER SYSTEM SET log_statement = 'all';
 SELECT pg_reload_conf();
